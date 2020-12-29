f18ae96e8f3a03e7b5630e038d4f13618b372eba
Leon Cheng <leonc@mit.edu>
2020-12-29 17:14:50 -0500

    add flipdown library and make it work basics

--- src/index.js
+++ src/index.js
@@ -9,3 +9,7 @@ import "./assets/js/browser.min.js";
 import "./assets/js/breakpoints.min.js";
 import "./assets/js/util.js";
 import "./assets/js/main.js";
+
+import "./flipdown/src/flipdown.css";
+import {FlipDown} from "./flipdown/src/flipdown";
+new FlipDown(3538137672).start();
